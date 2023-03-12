69523592981ac656ae71f74201ccdeca53467174
Shivangi <shivangit1111@gmail.com>
2023-03-12 17:46:59 +0530

    feat: webViewSetUp

--- app/src/main/java/com/example/webview/MainActivity.kt
+++ app/src/main/java/com/example/webview/MainActivity.kt
@@ -1,23 +1,38 @@
 package com.example.webview
 
+import android.annotation.SuppressLint
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.annotation.RequiresApi
 import com.example.webview.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val webview = binding.webView
-        webView(webview)
+        val webViewVariable = binding.webView
+        webViewSetUp(webViewVariable)
     }
 
-    private fun webView(webView: WebView) {
+    @SuppressLint("SetJavaScriptEnabled")
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun webViewSetUp(webView: WebView) {
+
+        webView.webViewClient = WebViewClient()
+        webView.apply {
+
+            settings.javaScriptEnabled = true
+            settings.safeBrowsingEnabled = true
+            loadUrl("http://saumyasingh.netlify.app/")
+        }
 
     }
 }
